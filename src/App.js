f0590f5974e70df75d46dd54ec25f0d5bd3d9230
csp <cssmiles@gmail.com>
2016-12-22 18:12:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ class App extends Component {
       render() {
           const authurl = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77arqe48ycj44b&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth&state=2522abcde12345&scope=r_basicprofile";
 
-
-
           return (
       <div className="App">
 
